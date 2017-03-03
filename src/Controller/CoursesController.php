7e0f2e1c71cb0c03e16c4189a1dfee98c0422e0e
Amar <amar@incaendo.com>
2017-03-03 17:28:27 +0530

    Added seCourseLogo funnctionality

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -5,6 +5,7 @@ use App\Controller\AppController;
 use Cake\ORM\TableRegistry;
 use Cake\I18n\Time;
 use Cake\Core\Exception\Exception;
+use Cake\Routing\Router;
 //use Cake\Datasource\ConnectionManager;
 
 /**
@@ -96,7 +97,41 @@ class CoursesController extends AppController{
         ));
     } 
 
-    
-
+    /*
+     * Desc – Service to update course logo to Amazons3 and return cdn url.
+     */
+    public function setCourseLogo() {
+      $response = FALSE;
+      $url = '';
+      try {
+        if ($this->request->is(['post'])) {
+          $file = $this->request->data['logo'];
+          $course_code = $this->request->data['course_code'];
+          if (!empty($file['name']) && !empty($course_code)) {
+            $file_name = time() . '_' .$file['name'];
+            $upload_path = 'img/logo/';
+            $upload_file = $upload_path.$file_name;
+            if (move_uploaded_file($file['tmp_name'], WWW_ROOT . $upload_file)) {
+              $course = $this->Courses->find()->where(['course_code' => $course_code]);
+              $fields = array();
+              foreach ($course as $fields) {
+                $fields->logo = $upload_file;
+              }
+              if (!empty($fields) && $this->Courses->save($fields)) {
+                $response = TRUE;
+                $url = Router::url('/', true) . $upload_file;
+              }
+            }
+          }
+        }
+      } catch (Exception $e) {
+        $this->log($e->getMessage() . '('. __METHOD__.')','error');
+      }
+      $this->set(array(
+        'response' => $response,
+        'url' => $url,
+        '_serialize' => ['response', 'url']
+      ));
+    }
    
 }
