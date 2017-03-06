f2ccce39d3e27d6181a7ad471afdb0072c7efa14
Amar <amar@incaendo.com>
2017-03-06 15:49:55 +0530

    Added course content table Model

---
+++ src/Model/Table/CourseContentsTable.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+
+class CourseContentsTable extends Table{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config){
+        
+        $this->table('course_contents');
+        $this->displayField('id');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+
+        $this->belongsTo('Courses', [
+            'foreignKey' => 'course_id',
+            'joinType'  =>  'INNER'
+        ]);          
+    }
+
+  
+}
