1be461a5c1dc6810b363aa986aa6157680605fbc
Amar <amar@incaendo.com>
2017-03-08 18:15:38 +0530

    getUserAttemptsToQuiz

--- src/Controller/ExamsController.php
+++ src/Controller/ExamsController.php
@@ -6,7 +6,7 @@ use Cake\ORM\TableRegistry;
 use Cake\I18n\Time;
 use Cake\Core\Exception\Exception;
 use Cake\Routing\Router;
-//use Cake\Datasource\ConnectionManager;
+use Cake\Datasource\ConnectionManager;
 
 /**
  * Users Controller
@@ -36,7 +36,33 @@ class ExamsController extends AppController{
     }
 
 
+    /**
+     * Desc – Service to get all the attempts made by user for quiz.
+     * Request –  String<quizID>, Int<UUID>
+     */
+    public function getUserAttemptsToQuiz() {
+      $data['count'] = '';
+      try {
+        if ($this->request->is('post')) {
+          $quiz_id = $this->request->data['quiz_id'];
+          $user_id = $this->request->data['user_id'];
+          $connection = ConnectionManager::get('default');
+          $sql="SELECT COUNT(user_quiz_responses.user_id) FROM user_quiz_responses INNER JOIN quiz_items on"
+            . " user_quiz_responses.item_id=quiz_items.item_id  INNER JOIN exam_sections ON"
+            . " quiz_items.quiz_id=exam_sections.exam_id where exam_sections.exam_id='$quiz_id' AND user_quiz_responses.user_id='$user_id'";
+          $results = $connection->execute($sql)->fetchAll('num');
+          foreach ($results as $result) {
+            $data['count']  = @end($result);
+          }
+        }
+      } catch (Exception $ex) {
+        $this->log($ex->getMessage());
+      }
 
+      $this->set(array(
+        'data' => $data,
+        '_serialize' => ['data']
+      ));
+    }
 
-   
 }
