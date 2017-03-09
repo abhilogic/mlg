b5249db41d08bab0c9b0e6225abdbf8b5d5f6bbe
Amar <amar@incaendo.com>
2017-03-09 15:52:02 +0530

    getUserAttemptDetailsByQuizID

--- src/Controller/ExamsController.php
+++ src/Controller/ExamsController.php
@@ -24,6 +24,7 @@ class ExamsController extends AppController{
     /** Index method    */
     public function index() {
 
+        $data['courses'] = array();
         $courses = $this->Exams->find('all')->toArray();
         foreach($courses as $course){
             $data['courses'][]= $course;
@@ -47,9 +48,11 @@ class ExamsController extends AppController{
           $quiz_id = $this->request->data['quiz_id'];
           $user_id = $this->request->data['user_id'];
           $connection = ConnectionManager::get('default');
-          $sql="SELECT COUNT(user_quiz_responses.user_id) FROM user_quiz_responses INNER JOIN quiz_items on"
-            . " user_quiz_responses.item_id=quiz_items.item_id  INNER JOIN exam_sections ON"
-            . " quiz_items.quiz_id=exam_sections.exam_id where exam_sections.exam_id='$quiz_id' AND user_quiz_responses.user_id='$user_id'";
+          $sql="SELECT COUNT(user_quiz_responses.user_id)"
+            . " FROM user_quiz_responses"
+            . " INNER JOIN quiz_items ON user_quiz_responses.item_id=quiz_items.item_id"
+            . " INNER JOIN exam_sections ON quiz_items.exam_section_id=exam_sections.id"
+            . " WHERE exam_sections.exam_id='$quiz_id' AND user_quiz_responses.user_id='$user_id'";
           $results = $connection->execute($sql)->fetchAll('num');
           foreach ($results as $result) {
             $data['count']  = @end($result);
@@ -65,4 +68,48 @@ class ExamsController extends AppController{
       ));
     }
 
+    /**
+     * Desc – Service to get question by question review of quiz attempted by user.
+     * Request –  String<quizID>, Int<UUID>
+     */
+    public function getUserAttemptDetailsByQuizID() {
+      try {
+        if ($this->request->is('post')) {
+          $data = array();
+          $quiz_id = $this->request->data['quiz_id'];
+          $user_id = $this->request->data['user_id'];
+          $connection = ConnectionManager::get('default');
+          $sql = "SELECT user_quiz_responses.*, quiz_items.exam_section_id, exam_sections.description"
+            . " FROM user_quiz_responses"
+            . " INNER JOIN quiz_items on user_quiz_responses.item_id=quiz_items.item_id"
+            . " INNER JOIN exam_sections ON quiz_items.exam_section_id=exam_sections.id"
+            . " WHERE exam_sections.exam_id='$quiz_id' AND user_quiz_responses.user_id='$user_id'";
+          $results = $connection->execute($sql)->fetchAll('assoc');
+          $i = 0;
+          foreach ($results as $result) {
+            if($i++ === 0) {
+              $data[$result['exam_section_id']]  = [
+                'section_id' => $result['exam_section_id'],
+                'description' => $result['description'],
+              ];
+            }
+            $data[$result['exam_section_id']]['questions'][] = [
+              'qId' => $result['item_id'],
+              'option' => $result['response'],
+              'timeTaken' => $result['time_taken'],
+              'skip' => $result['skip_count'],
+              'score' => $result['score'],
+              'askedForHint' => 'yes',
+            ];
+          }
+        }
+      } catch (Exception $ex) {
+        $this->log($ex->getMessage());
+      }
+
+      $this->set(array(
+        'data' => $data,
+        '_serialize' => ['data']
+      ));
+    }
 }
