60b4f89019db9a761a8e0cd24428f189280ea22d
amarnegi07 <amar@incaendo.com>
2017-05-08 17:22:39 +0530

    Added constant file

---
+++ common/constant.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * file is use to store constant regarding MLG.
+ */
+
+//Defined Roles and their Values.
+
+//Admin role
+define('ADMIN_ROLE_ID', 1);
+
+//parent role
+define('PARENT_ROLE_ID', 2);
+
+//teacher role
+define('TEACHER_ROLE_ID', 3);
+
+//student role
+define('STUDENT_ROLE_ID', 4);
+
+//guest teacher role
+define('GUEST_TEACHER_ROLE_ID', 5);
+
+//principal role
+define('PRINCIPAL_ROLE_ID', 6);
